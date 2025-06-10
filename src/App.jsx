5ca48134586cb1eca871b539230d876be528f25c
MuhammadMirzo <your-email@example.com>
2025-06-10 10:56:32 +0500

    Update: Changed Hello World text

--- src/App.jsx
+++ src/App.jsx
@@ -2,7 +2,12 @@ import React from "react";
 import "./App.css";
 
 const App = () => {
-  return <div className="">Hello world</div>;
+  return (
+    <div className="">
+      <h1>Hello world</h1>
+      <h1>Hello World Updated</h1>
+    </div>
+  );
 };
 
 export default App;
